7cf22cda46d947e77c22e5dffb29819f189e4e6b
natan1563 <natromafe@gmail.com>
2023-09-06 22:37:41 -0300

    add home appbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Sesab APP</h1> <!-- Criar um Header -->
+  <v-app-bar :elevation="3" class="px-5">
+      <div class="app-bar">
+        <h2>SESAB</h2>
+        <img src="@/assets/sesab.png" :width="90"/>
+      </div>
+  </v-app-bar>
   <v-container>
     <HomeComponent />
   </v-container>
@@ -8,3 +13,18 @@
 <script setup>
   import HomeComponent from '@/components/HomeComponent.vue'
 </script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Jost:wght@400;700&display=swap');
+  .app-bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  h2 {
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    letter-spacing: 0.3em;
+  }
+</style>
