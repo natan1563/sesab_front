1f5808430780197ca2963d3fb93f5eaf5dbf49e9
natan1563 <natromafe@gmail.com>
2023-09-06 22:37:46 -0300

    add mdi icons

--- src/main.js
+++ src/main.js
@@ -9,11 +9,16 @@ import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
+import mdiVue from "mdi-vue/v3";
+import * as mdijs from "@mdi/js";
 
 // Plugins
 import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
+app.use(mdiVue, {
+  icons: mdijs,
+});
 
 registerPlugins(app)
 
